e9a4f50b47069e711a43087f813265a82702ce71
Sarah Rossy <sarahrossysings@gmail.com>
2021-01-27 18:05:30 +0000

    images

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+$('#tweet-text').on("input", function(){
+  // console.log(this.value); // this is the DOM element
+  const $tweetText = $(this);
+  const form = $tweetText.closest('form');
+  const counter = form.find('.counter');
+  const tweet = $tweetText.val().length;
+  const charCounter = (140 - tweet);
+  //innerHTML = granted edit access!
+  counter.html(charCounter);
+  
+  if (charCounter < 0) {
+    counter.addClass('redText');
+  } else {
+    counter.removeClass('redText');
+  }
+})
+
+
+});
